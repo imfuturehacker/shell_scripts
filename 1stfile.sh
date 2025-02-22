ebcc4187c9a25314fa750804d43e4bf692412d6f
Ubuntu <ubuntu@ip-172-31-4-91.ap-south-1.compute.internal>
2025-02-22 15:21:21 +0000

    file has been committed

---
+++ 1stfile.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+
+name="bhavesh"
+company="CNX."
+
+
+#user defined valiables
+#echo "hello dosto my name is $name currently I am working in $company"
+
+#pre-defined variables
+#echo "the current loggedin user is $USER"
+
+
+#user-input valiables
+#read -p "Help me with your full name please?" fullname
+
+#echo "My full name is $fullname."
+
+
+
+
+echo "filename: $0"
+echo "Your name: $1"
+echo "Company name: $2"
+echo "number of arguments passed are: $#"
+
+
+
